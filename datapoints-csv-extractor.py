5a260da4f80d6899e992ce2b262b5a9038bd4887
Sam Feng <samantha.feng@cognite.com>
2019-02-18 14:11:44 +0100

    Script refactored by Even

--- datapoints-csv-extractor.py
+++ datapoints-csv-extractor.py
@@ -1,107 +1,109 @@
-import glob
+import logging
 import os
-import re
+import sys
 import time
+from logging.handlers import TimedRotatingFileHandler
+from operator import itemgetter
+from pathlib import Path
 
-import pandas as pd
+import pandas
 from cognite import CogniteClient
-from cognite.client.stable.datapoints import TimeseriesWithDatapoints, Datapoint
+from cognite.client.stable.datapoints import Datapoint, TimeseriesWithDatapoints
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(name)s %(levelname)s - %(message)s",
+    handlers=[
+        TimedRotatingFileHandler("extractor.log", when="midnight", backupCount=7),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+logger = logging.getLogger()
 
 
-API_KEY = os.environ.get('COGNITE_EXTRACTOR_API_KEY')
+API_KEY = os.environ.get("COGNITE_EXTRACTOR_API_KEY")
 if not API_KEY:
-    print('COME ON JAN&SAM, YOU FORGOT THE API KEY!')
+    print("COME ON JAN&SAM, YOU FORGOT THE API KEY!")
     sys.exit(2)
 
 
 # Global variable for last timestamp processed
-last_processed = 1548975600
+LAST_PROCESSED_TIMESTAMP = 1550076300
 
 # Maximum number of time series batched at once
-batch_max = 1000
+BATCH_MAX = 1000
 
 # Path to folder of CSV files
-folder_path = '../TebisSampleData2/'
+FOLDER_PATH = "../TebisSampleData2/"
 
-# File path of files processed. Stores Epoch timestamp
-epoch_path = 'epoch_timestamp.txt'
 
-client = CogniteClient(api_key=API_KEY)
+def post_datapoints(client, paths, existing_timeseries):
+    current_time_series = []  # List of time series being processed
 
-# Input: a list of absolute or relative file paths of CSVs containing datapoints to process
-def post_datapoints(files):
-    # List of time series being processed
-    current_time_series = []
+    def post_datapoints():
+        nonlocal current_time_series
+        client.datapoints.post_multi_time_series_datapoints(current_time_series)
+        current_time_series = []
 
-    # Loop through all CSV folders, posting all datapoints in the file
-    for filename in files:
-        # Read CSV, removing first row containing units of data
-        df = pd.read_csv(os.path.join(folder_path, filename), encoding='latin-1', delimiter=';', quotechar='"', skiprows=[1], index_col=0)
-        indices_list = df.index.tolist()
+    def convert_float(value_str):
+        return float(value_str.replace(",", "."))
+
+    for path in paths:
+        df = pandas.read_csv(path, encoding="latin-1", delimiter=";", quotechar='"', skiprows=[1], index_col=0)
+        timestamps = [int(o) * 1000 for o in df.index.tolist()]
+        count_of_data_points = 0
 
         for col in df:
-            # Post datapoints if list of time series being processed is greater than the maximum, then empty list
-            if len(current_time_series) >= batch_max:
-                client.datapoints.post_multi_time_series_datapoints(current_time_series)
-                current_time_series = []
+            if len(current_time_series) >= BATCH_MAX:
+                post_datapoints()
 
-            values_list = df[col].tolist()
-            datapoints_list = []
-            time_series = str(col.rpartition(':')[2].strip())
+            name = str(col.rpartition(":")[2].strip())
 
-            for i in range(len(values_list)):
-                # Format Datapoint and TimeseriesWithDatapoints
-                if pd.notnull(values_list[i]):
-                    value = float(values_list[i].replace(',', '.'))
-                    datapoints_list.append(Datapoint(timestamp=int(indices_list[i])*1000, value=value))
-            if datapoints_list:
-                current_time_series.append(TimeseriesWithDatapoints(name=time_series, datapoints=datapoints_list))
+            if name in existing_timeseries:
+                data_points = []
 
-        # If list of datapoints being processed is not empty, post leftover datapoints
+                for i, value in enumerate(df[col].tolist()):
+                    if pandas.notnull(value):
+                        data_points.append(Datapoint(timestamp=timestamps[i], value=convert_float(value)))
+
+                if data_points:
+                    current_time_series.append(TimeseriesWithDatapoints(name=name, datapoints=data_points))
+                    count_of_data_points += len(data_points)
+
         if current_time_series:
-            client.datapoints.post_multi_time_series_datapoints(current_time_series)
-            current_time_series = []
+            post_datapoints()
 
-        os.remove(filename)
-        print('Processed: ' + filename)
+        logger.info("Processed {} datapoints from {}".format(count_of_data_points, path))
 
-    # Update the timestamp compared to if new files are processed
-    global last_processed
-    if files:
-        last_processed = int(re.findall(r'\d+', files[-1])[-1])
-        with open(epoch_path, 'a') as f:
-            f.write(str(last_processed) + '\n')
+    return max(path.stat().st_mtime for path in paths)  # Timestamp of most recent modified path
 
-    # Delay processing between files by a second
-    time.sleep(1)
 
-# Find new files, if existent and post datapoints from that file
-def find_new_files():
-    # Find all files in filepath and their corresponding timestamps
-    all_files = sorted(glob.glob(folder_path + '/*.csv'))
-    all_timestamps = [re.findall(r'\d+', x)[-1] for x in all_files]
-    all_timestamps = list(map(int, all_timestamps))
+def find_new_files(last_mtime, base_path):
+    paths = [(p, p.stat().st_mtime) for p in Path(base_path).glob("*.csv")]
+    paths.sort(key=itemgetter(1), reverse=True)  # Process newest file first
+    return [p for p, mtime in paths if mtime > last_mtime]
 
-    # Find files from a timestamp later than the last processed file
-    global last_processed
-    with open(epoch_path) as f:
-        for line in f:
-            pass
-        last_line = line
-        last_processed = int(last_line)
-    new_timestamps = [i for i, x in enumerate(all_timestamps) if x > last_processed]
-    new_files = [all_files[i] for i in new_timestamps]
-
-    if new_files:
-        post_datapoints(new_files)
-
-    time.sleep(5)
 
 def extract_datapoints():
-    initial_files = sorted(glob.glob(folder_path + '/*.csv'))
-    post_datapoints(initial_files)
-    while True:
-        find_new_files()
+    client = CogniteClient(api_key=API_KEY)
+    existing_timeseries = set(i["name"] for i in client.time_series.get_time_series(autopaging=True).to_json())
 
-if __name__ == '__main__':
+    last_timestamp = LAST_PROCESSED_TIMESTAMP
+
+    try:
+        while True:
+            paths = find_new_files(last_timestamp, FOLDER_PATH)
+            if paths:
+                last_timestamp = post_datapoints(client, paths, existing_timeseries)
+
+                # logger.info("Removing processed files {}".format(', '.join(p.name for p in paths)))
+                # for path in paths:
+                #    path.unlink()
+
+                time.sleep(5)
+    except KeyboardInterrupt:
+        logger.warning("Extractor stopped")
+
+
+if __name__ == "__main__":
     extract_datapoints()
