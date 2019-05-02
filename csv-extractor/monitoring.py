e4d9d5427769911da0ef3d54fe46098e2716776d
Even Wiik Thomassen <even.thomassen@cognite.com>
2019-05-02 18:43:24 +0200

    Simplified monitoring class, removed metrics configuration code duplication.
    Signed-off-by: Even Wiik Thomassen <even.thomassen@cognite.com>

--- csv-extractor/monitoring.py
+++ csv-extractor/monitoring.py
@@ -34,71 +34,54 @@ def configure_prometheus(live: bool, project_name):
 class Prometheus:
     labels = ["project_name"]
 
-    def __init__(self, prometheus, live: bool, project_name: str):
+    def __init__(self, prometheus, live: bool, project_name: str, registry=None):
         self.project_name = project_name
         self.prometheus = prometheus
         self.namespace = "csv_live" if live else "csv_hist"
         self.label_values = {self.project_name}
+        self.registry = CognitePrometheus.registry if registry is None else registry
 
         self.info = Info("host", "Host info", namespace=self.namespace, registry=CognitePrometheus.registry)
         self.info.info({"hostname": socket.gethostname(), "fqdn": socket.getfqdn()})
         self.process = ProcessCollector(namespace=self.namespace, registry=CognitePrometheus.registry)
         self.platform = PlatformCollector(registry=CognitePrometheus.registry)
 
-        self.created_time_series_counter = Counter(
-            "created_time_series_total",
-            "Number of time series created since the extractor started running",
-            namespace=self.namespace,
-            labelnames=self.labels,
-            registry=CognitePrometheus.registry,
-        ).labels(*self.label_values)
+        self.created_time_series_counter = self._create_metric(
+            Counter, "created_time_series_total", "Number of time series created since the extractor started running"
+        )
 
-        self.all_data_points_counter = Counter(
-            "posted_data_points_total",
-            "Number of datapoints posted since the extractor started running",
-            namespace=self.namespace,
-            labelnames=self.labels,
-            registry=CognitePrometheus.registry,
-        ).labels(*self.label_values)
+        self.all_data_points_counter = self._create_metric(
+            Counter, "posted_data_points_total", "Number of datapoints posted since the extractor started running"
+        )
 
-        self.count_of_time_series_gauge = Gauge(
-            "posted_time_series_count",
-            "Number of timeseries that had valid datapoints in the current file",
-            namespace=self.namespace,
-            labelnames=self.labels,
-            registry=CognitePrometheus.registry,
-        ).labels(*self.label_values)
+        self.count_of_time_series_gauge = self._create_metric(
+            Gauge, "posted_time_series_count", "Number of timeseries that had valid datapoints in the current file"
+        )
 
-        self.processing_failed_counter = Counter(
+        self.processing_failed_counter = self._create_metric(
+            Counter,
             "failed_processing_files",
             "Number of files where processing failed since the extractor started running",
-            namespace=self.namespace,
-            labelnames=self.labels,
-            registry=CognitePrometheus.registry,
-        ).labels(*self.label_values)
+        )
 
-        self.available_csv_files_gauge = Gauge(
-            "available_csv_files",
-            "Number of csv files in the folder that could be processed by the extractor",
-            namespace=self.namespace,
-            labelnames=self.labels,
-            registry=CognitePrometheus.registry,
-        ).labels(*self.label_values)
+        self.available_csv_files_gauge = self._create_metric(
+            Gauge, "available_csv_files", "Number of csv files in the folder that could be processed by the extractor"
+        )
 
-        self.unprocessed_files_gauge = Gauge(
-            "unprocessed_files",
-            "Number of csv files that remains to be processed in this batch",
-            namespace=self.namespace,
-            labelnames=self.labels,
-            registry=CognitePrometheus.registry,
-        ).labels(*self.label_values)
+        self.unprocessed_files_gauge = self._create_metric(
+            Gauge, "unprocessed_files", "Number of csv files that remains to be processed in this batch"
+        )
 
-        self.successfully_processed_files_gauge = Gauge(
+        self.successfully_processed_files_gauge = self._create_metric(
+            Gauge,
             "successfully_processed_files",
             "Number of csv files that has been successfully processed in this batch",
-            namespace=self.namespace,
-            labelnames=self.labels,
-            registry=CognitePrometheus.registry,
+        )
+
+    def _create_metric(self, metric_class, name, description):
+        """Create a new metric of 'metric_class' with 'name' and 'description'."""
+        return metric_class(
+            name, description, namespace=self.namespace, labelnames=self.labels, registry=self.registry
         ).labels(*self.label_values)
 
     def incr_failed_files_counter(self, amount: int = 1) -> None:
