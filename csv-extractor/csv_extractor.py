ad1ca906f4ca090c0632c9638951d358d1b4f62c
kozhevnykov <oleksandr.kozhevnykov@cognite.com>
2020-02-24 11:38:04 +0100

    fix bug with thread and copying time_series

--- csv-extractor/csv_extractor.py
+++ csv-extractor/csv_extractor.py
@@ -123,8 +123,9 @@ def process_csv_file(client, monitor, csv_path, existing_time_series):
     for col_name, v in parsed_file.items():
         if len(current_time_series) >= 1000:
             network_threads.append(
-                threading.Thread(target=_log_error, args=(client.datapoints.insert_multiple, current_time_series))
-            )
+                threading.Thread(target=_log_error,
+                                 args=(client.datapoints.insert_multiple, current_time_series.copy()))
+            )  # make copy of time_series to use in Treads safely
 
             current_time_series.clear()
 
